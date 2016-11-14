8238dfb88ff8d0763a5d541f3af7e81f86e18d39
Tom Wolff <tomwolff093@gmail.com>
2016-11-14 11:47:39 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,11 @@
 <?php
 
-good_username = "bob";
-good_password = "super-secret";
+$good_username="bob";
+$good_password="super-secret";
 
-// Remplacer False par une expression booléenne pour tester les paramètres reçus
-if (False) {
+if ($good_username == $_POST["username"] && $good_password == $_POST["password"]) {
   echo "Vous etes connecté !";
-} else {
-  echo "Mauvais nom d'utilisateur ou mot de passe."
 }
-
-?>
+else {
+  echo "Mauvais nom d'utilisateur ou mot de passe.";
+}
